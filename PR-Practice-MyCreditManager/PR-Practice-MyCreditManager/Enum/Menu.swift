5a11e2d91663f7b2d31e7705533f8e33a76f4d01
devxsby <devxsby@gmail.com>
2022-12-20 10:28:45 +0900

    Feat: 학생 관리 프로그램 1차 구현

---
+++ PR-Practice-MyCreditManager/PR-Practice-MyCreditManager/Enum/Menu.swift
@@ -0,0 +1,17 @@
+//
+//  Menu.swift
+//  PR-Practice-MyCreditManager
+//
+//  Created by devxsby on 2022/12/20.
+//
+
+import Foundation
+
+enum Menu: String {
+    case addStudent = "1"
+    case deleteStudent = "2"
+    case addOrChangeGrades = "3"
+    case deleteGrades = "4"
+    case showGrades = "5"
+    case exitProgram = "X"
+}
---
+++ PR-Practice-MyCreditManager/PR-Practice-MyCreditManager/Model/Student.swift
@@ -0,0 +1,17 @@
+//
+//  Student.swift
+//  PR-Practice-MyCreditManager
+//
+//  Created by devxsby on 2022/12/20.
+//
+
+import Foundation
+
+class Student {
+    let name: String
+    var grades = [String : String]()
+    
+    init(name: String) {
+        self.name = name
+    }
+}
