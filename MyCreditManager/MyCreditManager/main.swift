0e425b54d573c5afa5c8b9c20f53783dbc3eab82
zhunhe <xuzhunhe@gmail.com>
2022-12-20 10:18:31 +0900

    refactor: Change getInput()
    - Change function name getInput()-> getLine()
    - Add parameter (messageType: Message)

--- MyCreditManager/MyCreditManager/main.swift
+++ MyCreditManager/MyCreditManager/main.swift
@@ -13,8 +13,8 @@ enum State {
 }
 
 // 설명문구 출력 후 readLine() 리턴
-func getInput() -> String? {
-    printMessage(messageType: .menu)
+func getLine(messageType: Message) -> String? {
+    printMessage(messageType: messageType)
     return readLine()
 }
 
@@ -22,7 +22,7 @@ func runProgram() {
     var state: State = .run
 
     // EOF(Ctrl + D)는 종료로 처리
-    while state != .quit, let input = getInput() {
+    while state != .quit, let input = getLine(messageType: .menu) {
         // enum에 명시하지 않은 명령어는 에러로 처리
         guard let command = Command(rawValue: input) else {
             printMessage(messageType: .menuError)
